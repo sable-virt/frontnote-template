824dc808de46e1f2f101612957b5ff8c20c4f654
frontainer <frontainer@gmail.com>
2014-08-24 18:50:22 +0900

    ツールチップ実装

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,32 +1,15 @@
 (function() {
+    var TOOLTIP_OFFSET_X = 10,
+        TOOLTIP_OFFSET_Y = 10,
+        TOOLTIP_DURATION = 200,
+        PAGETOP_DURATION = 450;
 
     var $window = $(window),
+        $document = $(document),
         $body = $(document.body),
         $pagetop = $('.fn-pagetop');
     var isShow = false;
 
-    $('.fn-menu, .fn-bars').rippleEffect();
-    $('.fn-drawer-trigger').on('click', function(e) {
-        e.preventDefault();
-        if($body.hasClass('fn-overflow')) {
-            hideDrawer();
-        } else {
-            showDrawer();
-        }
-    });
-    $('.fn-content').on('touchstart mousedown', function(e) {
-        if($body.hasClass('fn-overflow')) {
-            hideDrawer();
-        }
-    });
-    $pagetop.on('click', function(e) {
-        e.preventDefault();
-        $pagetop.removeClass('fn-show');
-        $('html,body').animate({
-            scrollTop: 0
-        },500,'swing');
-
-    });
     $window.on('resize', function(e) {
         var width = $window.width();
         if (width > 680 && $body.hasClass('fn-overflow')) {
@@ -45,9 +28,6 @@
         }
     });
 
-    $(document).on('blur','.fn-code', uneditable);
-    $(document).on('click','.fn-pre .fn-icon', editable);
-
     function uneditable() {
         $(this).attr('contentEditable',false);
     }
@@ -65,6 +45,44 @@
 
     function init() {
         $window.trigger('mousewheel');
+        $document.on('blur','.fn-code', uneditable);
+        $document.on('click','.fn-pre .fn-icon', editable);
+        $('.fn-menu, .fn-bars').rippleEffect();
+        $('.fn-drawer-trigger').on('click', function(e) {
+            e.preventDefault();
+            if($body.hasClass('fn-overflow')) {
+                hideDrawer();
+            } else {
+                showDrawer();
+            }
+        });
+        $('.fn-content').on('touchstart mousedown', function(e) {
+            if($body.hasClass('fn-overflow')) {
+                hideDrawer();
+            }
+        });
+
+        var $tooltip = $('<div class="fn-tooltip"><i class=""></i></div>');
+        $body.append($tooltip);
+        $('.fn-preview > *').on('mousemove',function(e) {
+            $tooltip.html(this.className);
+            $tooltip.stop().fadeIn(TOOLTIP_DURATION).css({
+                top: e.clientY + TOOLTIP_OFFSET_Y,
+                left: e.clientX + TOOLTIP_OFFSET_X
+            });
+        }).on('mouseleave',function(e) {
+            $tooltip.stop().fadeOut(TOOLTIP_DURATION);
+        });
+
+        $pagetop.on('click', function(e) {
+            e.preventDefault();
+            $pagetop.removeClass('fn-show');
+            $('html,body').animate({
+                scrollTop: 0
+            },PAGETOP_DURATION,'swing');
+
+        });
+        hljs.initHighlightingOnLoad();
     }
     function hideDrawer() {
         $body.removeClass('fn-overflow');
